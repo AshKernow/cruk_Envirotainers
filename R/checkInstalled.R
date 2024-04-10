fa21dc6409501d2ca4acbf09eedbf55716fdea50
AshKernow <Ashley.Sawle@cruk.cam.ac.uk>
2024-04-10 10:35:18 +0100

    Add R env

---
+++ R/checkInstalled.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(library(tidyverse))
+
+# Get list of expected packages from R.yaml
+
+pkgList <- yaml::yaml.load_file("R.yaml")$dependencies
+
+pks <- str_remove(pkgList, ".*-")
+
+# Check if a package is installed
+
+checkInstalled <- function(pkg) {
+    insPackages <- installed.packages() %>% rownames() %>% str_to_lower()
+    if (!pkg %in% insPackages) {
+        print(paste0(pkg, " is not installed"))
+    }
+}
+
+
+# Check if all packages are installed
+
+walk(pks, checkInstalled)
--- R/updateR.sh
+++ R/updateR.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 mamba env update --file R.yaml
+currDate=`date | sed 's/ /_/g'`
+mamba env config vars set --name R CreationDate=${currDate}
+
