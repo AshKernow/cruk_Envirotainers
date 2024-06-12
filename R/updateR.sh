d64de941531a680011591b65d3fe27a7f0fd1fb2
AshKernow <Ashley.Sawle@cruk.cam.ac.uk>
2024-06-12 10:28:34 +0100

    Add qdnaseq and version logs

--- R/updateR.sh
+++ R/updateR.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-mamba env update --file R.yaml
-currDate=`date | sed 's/ /_/g'`
-mamba env config vars set --name R CreationDate=${currDate}
+#mamba env update --file R.yaml
+#currDate=`date | sed 's/ /_/g'`
+#mamba env config vars set --name R CreationDate=${currDate}
 
+# Log the current versions of all software
+
+timeStamp=$(date +"%Y%m%d_%H%M%S")
+logFile=version_logs/versions_${timeStamp}.log
+
+echo `date` > ${logFile}
+echo >> ${logFile}
+conda list -n R >>  ${logFile}
+    
