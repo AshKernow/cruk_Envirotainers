8c09f436d7ef62ba00e5e2711583e1d81c8a008c
Ashley Sawle <ads2202cu@gmail.com>
2026-09-10 15:33:54 +0100

    Make 4.6 and add packages

--- conda_environments/RNAseq_analysis/xInstallPackages.sh
+++ conda_environments/RNAseq_analysis/xInstallPackages.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 # Run the installation script
-packages=($(grep -v "^#" PackagesToInstall.txt))
+# ENV_NAME, PKG_LIST, LOG_DIR and SUMMARY_LOG can be overridden via the
+# environment to target a different conda environment/package list (e.g.
+# the RNAseq_R4.6 build), while defaulting to today's RNAseq_R behaviour.
+ENV_NAME="${ENV_NAME:-RNAseq_R}"
+PKG_LIST="${PKG_LIST:-PackagesToInstall.txt}"
+logDir="${LOG_DIR:-AllPackageInstallationLogs}"
+summaryLog="${SUMMARY_LOG:-AllPackageInstallationSummary.log}"
+
+packages=($(grep -v "^#" "${PKG_LIST}"))
 numPackages=${#packages[@]}
-logDir="AllPackageInstallationLogs"
 mkdir -p ${logDir}
 for pkgNum in $(seq 0 $((numPackages - 1))); do
     pkg=${packages[$pkgNum]}
@@ -13,13 +20,13 @@ for pkgNum in $(seq 0 $((numPackages - 1))); do
     logFile="${logDir}/${numPad}_PackageInstallation_${pkgNam}.log"
     echo "Installing package: $pkg"
     echo "Installing package: $pkg" > ${logFile}
-	conda run -n RNAseq_R Rscript xrInstallPackage.R $pkg >> ${logFile} 2>&1
+	conda run -n "${ENV_NAME}" Rscript xrInstallPackage.R $pkg >> ${logFile} 2>&1
 done
 
-cat ${logDir}/*.log | 
+cat ${logDir}/*.log |
     grep "%%" |
     sed 's/%% //g' \
-    > AllPackageInstallationSummary.log
+    > "${summaryLog}"
 
 # Remove the directory created by "omnipathr"
  rm -rf omnipathr-log
